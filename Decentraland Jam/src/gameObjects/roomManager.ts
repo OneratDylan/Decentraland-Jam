784d129c74f316b31b40266b4e00aa5d2fad94c7
OneratDylan <dylanjwalker@oneratgames.com>
2020-09-06 22:09:29 +1000

    New room structure and random levels

--- Decentraland Jam/src/gameObjects/roomManager.ts
+++ Decentraland Jam/src/gameObjects/roomManager.ts
@@ -1,11 +1,9 @@
-import { Room_1 } from "./room_1"
-import { Room_2 } from "./room_2"
-import { Room_3 } from "./room_3"
-import { Room_4 } from "./room_4"
+import { CreateRoom } from "./rooms"
 
 let x: number = 24;
 let y: number = 24;
 let roomSize: number = 10;
+const input = Input.instance
 
 let room1: Entity
 let room2: Entity
@@ -17,24 +15,44 @@ let room7: Entity
 let room8: Entity
 let room9: Entity
 
+let roomPositions: Array<Vector3> = [new Vector3(x, 0, y),
+    new Vector3(x, 0, y - roomSize),
+    new Vector3(x, 0, y + roomSize),
+    new Vector3(x - roomSize, 0, y),
+    new Vector3(x + roomSize, 0, y),
+    new Vector3(x + roomSize, 0, y + roomSize),
+    new Vector3(x + roomSize, 0, y - roomSize),
+    new Vector3(x - roomSize, 0, y - roomSize),
+    new Vector3(x - roomSize, 0, y + roomSize)
+];
+
 export class roomManager extends Entity implements ISystem
 {
     constructor() {
         super();
         engine.addEntity(this)
 
-        new Room_1(new Vector3(x, 0, y)); //middle room
-        new Room_2(new Vector3(x, 0, y - roomSize)); // bottom room
-        new Room_3(new Vector3(x, 0, y + roomSize)); // top room
-        new Room_4(new Vector3(x - roomSize, 0, y)); // left room
-        new Room_2(new Vector3(x + roomSize, 0, y)); // right room
-        new Room_4(new Vector3(x + roomSize, 0, y + roomSize)); // top right
-        new Room_2(new Vector3(x + roomSize, 0, y - roomSize)); // bottom right
-        new Room_1(new Vector3(x - roomSize, 0, y - roomSize)); // bottom left
-        new Room_1(new Vector3(x - roomSize, 0, y + roomSize)); // top left
+        room1 = new CreateRoom(new Vector3(x, 0, y)); //middle room
+        room2 = new CreateRoom(new Vector3(x, 0, y - roomSize)); // bottom room
+        room3 = new CreateRoom(new Vector3(x, 0, y + roomSize)); // top room
+        room4 = new CreateRoom(new Vector3(x - roomSize, 0, y)); // left room
+        room5 = new CreateRoom(new Vector3(x + roomSize, 0, y)); // right room
+        room6 = new CreateRoom(new Vector3(x + roomSize, 0, y + roomSize)); // top right
+        room7 = new CreateRoom(new Vector3(x + roomSize, 0, y - roomSize)); // bottom right
+        room8 = new CreateRoom(new Vector3(x - roomSize, 0, y - roomSize)); // bottom left
+        room9 = new CreateRoom(new Vector3(x - roomSize, 0, y + roomSize)); // top left
     }
 
     update(dt: number) {
 
+        input.subscribe("BUTTON_DOWN", ActionButton.SECONDARY, false, (e) => {
+            engine.removeEntity(room1)
+            engine.removeEntity(room2)
+            engine.removeEntity(room3)
+            engine.removeEntity(room4)
+            engine.removeEntity(room5)
+            engine.removeEntity(room6)
+            engine.removeEntity(room7)
+        })
     }
 }
