d9a75540545491e9eac78b2870da2092cae5a943
OneratDylan <dylanjwalker@oneratgames.com>
2020-09-07 18:30:19 +1000

    All rooms change

--- Decentraland Jam/src/gameObjects/roomManager.ts
+++ Decentraland Jam/src/gameObjects/roomManager.ts
@@ -44,33 +44,97 @@ export class roomManager extends Entity implements ISystem
             if (room1.isInRoom == false) {
                 room1.stillInRoom = false
                 engine.removeEntity(room1);
-                room1 = new CreateRoom(roomPositions[0]); //middle room
+                room1 = new CreateRoom(roomPositions[0]); 
             }
         }
-        //if (room2.isInRoom == true) {
-        //    engine.removeEntity(room2);
-        //}
-        //if (room3.isInRoom == true) {
-        //    engine.removeEntity(room3);
-        //}
-        //if (room4.isInRoom == true) {
-        //    engine.removeEntity(room4);
-        //}
-        //if (room5.isInRoom == true) {
-        //    engine.removeEntity(room5);
-        //}
-        //if (room6.isInRoom == true) {
-        //    engine.removeEntity(room6);
-        //}
-        //if (room7.isInRoom == true) {
-        //    engine.removeEntity(room7);
-        //}
-        //if (room8.isInRoom == true) {
-        //    engine.removeEntity(room8);
-        //}
-        //if (room9.isInRoom == true) {
-        //    engine.removeEntity(room9);
-        //}
+        //------------------------------------------------------------//
+        if (room2.isInRoom == true) {
+            room2.stillInRoom = true;
+        }
+        if (room2.stillInRoom == true) {
+            if (room2.isInRoom == false) {
+                room2.stillInRoom = false
+                engine.removeEntity(room2);
+                room2 = new CreateRoom(roomPositions[1]); 
+            }
+        }
+        //------------------------------------------------------------//
+        if (room3.isInRoom == true) {
+            room3.stillInRoom = true;
+        }
+        if (room3.stillInRoom == true) {
+            if (room3.isInRoom == false) {
+                room3.stillInRoom = false
+                engine.removeEntity(room3);
+                room3 = new CreateRoom(roomPositions[2]); 
+            }
+        }
+        //------------------------------------------------------------//
+        if (room4.isInRoom == true) {
+            room4.stillInRoom = true;
+        }
+        if (room4.stillInRoom == true) {
+            if (room4.isInRoom == false) {
+                room4.stillInRoom = false
+                engine.removeEntity(room4);
+                room4 = new CreateRoom(roomPositions[3]); 
+            }
+        }
+        //------------------------------------------------------------//
+        if (room5.isInRoom == true) {
+            room5.stillInRoom = true;
+        }
+        if (room5.stillInRoom == true) {
+            if (room5.isInRoom == false) {
+                room5.stillInRoom = false
+                engine.removeEntity(room5);
+                room5 = new CreateRoom(roomPositions[4]); 
+            }
+        }
+        //------------------------------------------------------------//
+        if (room6.isInRoom == true) {
+            room6.stillInRoom = true;
+        }
+        if (room6.stillInRoom == true) {
+            if (room6.isInRoom == false) {
+                room6.stillInRoom = false
+                engine.removeEntity(room6);
+                room6 = new CreateRoom(roomPositions[5]); 
+            }
+        }
+        //------------------------------------------------------------//
+        if (room7.isInRoom == true) {
+            room7.stillInRoom = true;
+        }
+        if (room7.stillInRoom == true) {
+            if (room7.isInRoom == false) {
+                room7.stillInRoom = false
+                engine.removeEntity(room7);
+                room7 = new CreateRoom(roomPositions[6]);
+            }
+        }
+        //------------------------------------------------------------//
+        if (room8.isInRoom == true) {
+            room8.stillInRoom = true;
+        }
+        if (room8.stillInRoom == true) {
+            if (room8.isInRoom == false) {
+                room8.stillInRoom = false
+                engine.removeEntity(room8);
+                room8 = new CreateRoom(roomPositions[7]);
+            }
+        }
+        //------------------------------------------------------------//
+        if (room9.isInRoom == true) {
+            room9.stillInRoom = true;
+        }
+        if (room9.stillInRoom == true) {
+            if (room9.isInRoom == false) {
+                room9.stillInRoom = false
+                engine.removeEntity(room9);
+                room9 = new CreateRoom(roomPositions[8]);
+            }
+        }
     }
 
 
