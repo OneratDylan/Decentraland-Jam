bee6d4abbba10740d01e148f06b4a150f2572c77
OneratDylan <dylanjwalker@oneratgames.com>
2020-09-08 19:30:57 +1000

    Rooms now remove their childern

--- Decentraland Jam/src/gameObjects/rooms.ts
+++ Decentraland Jam/src/gameObjects/rooms.ts
@@ -19,21 +19,50 @@ export class CreateRoom extends Entity {
         this.num = Scalar.RandomRange(1, 4)
         this.num = Math.round(this.num)
 
+        //Room One
         if (this.num == 1) {
             this.addComponent(new GLTFShape("Models/Room_10x10_01.gltf"))
             this.addComponent(new Transform({ position: pos }))
+
+            const bed = new Entity()
+            engine.addEntity(bed)
+            bed.addComponent(new GLTFShape("Models/Obj_Bed.gltf"))
+            //bed.addComponent(new Transform({ position: pos }))
+            bed.setParent(this)
+
         }
+        //Room Two
         if (this.num == 2) {
             this.addComponent(new GLTFShape("Models/Room_10x10_02.gltf"))
             this.addComponent(new Transform({ position: pos }))
+
+            const couch = new Entity()
+            engine.addEntity(couch)
+            couch.addComponent(new GLTFShape("Models/Obj_Couch_Long_002.gltf"))
+            //couch.addComponent(new Transform({ position: pos }))
+            couch.setParent(this)
         }
+        //Room Three
         if (this.num == 3) {
             this.addComponent(new GLTFShape("Models/Room_10x10_03.gltf"))
             this.addComponent(new Transform({ position: pos }))
+
+            const desk = new Entity()
+            engine.addEntity(desk)
+            desk.addComponent(new GLTFShape("Models/Obj_DeskStudy.gltf"))
+            //desk.addComponent(new Transform({ position: pos }))
+            desk.setParent(this)
         }
+        //Room Four
         if (this.num == 4) {
             this.addComponent(new GLTFShape("Models/Room_10x10_04.gltf"))
             this.addComponent(new Transform({ position: pos }))
+
+            const KCounter = new Entity()
+            engine.addEntity(KCounter)
+            KCounter.addComponent(new GLTFShape("Models/Obj_KitchenCounter.gltf"))
+            //KCounter.addComponent(new Transform({ position: pos }))
+            KCounter.setParent(this)
         }
 
 
